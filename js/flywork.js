41480ed225535994f81c340b0b27c0ae6e294cdf
Silvio Delgado <silviomdelgado@gmail.com>
2020-03-13 00:04:21 -0300

    Refactoring do AMD

--- js/flywork.js
+++ js/flywork.js
@@ -9,67 +9,75 @@
 })(typeof global !== "undefined" ? global : this.window || this.global, function (root) {
     'use strict';
 
-    const chk = document.querySelectorAll('.flywork-checkbox [type="checkbox"]');
-    chk.forEach(function (el, i) {
-        const id = el.getAttribute('id');
-        el.addEventListener('change', function (evt) {
-            let lbl = document.querySelector('label[for="' + id + '"]');
-            lbl.classList.toggle('checked');
-        });
-    });
+    const flywork_init = function () {
 
-    const rb = document.querySelectorAll('.flywork-radio [type="radio"]');
-    rb.forEach(function (el, i) {
-        el.addEventListener('change', function (evt) {
-            const name = this.getAttribute('name');
-            const id = this.getAttribute('id');
+        let chk = document.querySelectorAll('.flywork-checkbox [type="checkbox"]');
+        let rb = document.querySelectorAll('.flywork-radio [type="radio"]');
+        let elems = document.querySelectorAll('.flywork-radio [type="radio"]:checked, .flywork-checkbox [type="checkbox"]:checked');
 
-            let inputs = document.querySelectorAll('input[name="' + name + '"]');
-            inputs.forEach(function (item, j) {
-                const el_id = item.getAttribute('id');
-                item.removeAttribute('checked');
-                let lbl = document.querySelector('label[for="' + el_id + '"]');
-                lbl.classList.remove('checked');
+        chk.forEach(function (el, i) {
+            let id = el.getAttribute('id');
+            el.addEventListener('change', function (evt) {
+                let lbl = document.querySelectorAll('label[for="' + id + '"]');
+                lbl[0].classList.toggle('checked');
             });
+        });
 
-            let el = document.querySelector('#' + id);
-            el.setAttribute('checked', true);
-            let lbl = document.querySelector('label[for="' + id + '"]');
+        rb.forEach(function (el, i) {
+            el.addEventListener('change', function (evt) {
+                let name = this.getAttribute('name');
+                let id = this.getAttribute('id');
+
+                let inputs = document.querySelectorAll('input[name="' + name + '"]');
+                inputs.forEach(function (item, j) {
+                    let el_id = item.getAttribute('id');
+                    item.removeAttribute('checked');
+                    let lbl = document.querySelector('label[for="' + el_id + '"]');
+                    lbl.classList.remove('checked');
+                });
+
+                let el = document.querySelector('#' + id);
+                el.setAttribute('checked', true);
+                let lbl = document.querySelector('label[for="' + id + '"]');
+                lbl.classList.add('checked');
+            });
+        });
+
+        elems.forEach(function (el, i) {
+            let el_id = el.getAttribute('id');
+            let lbl = document.querySelector('label[for="' + el_id + '"]');
             lbl.classList.add('checked');
         });
-    });
 
-    let elems = document.querySelectorAll('.flywork-radio [type="radio"]:checked, .flywork-checkbox [type="checkbox"]:checked');
-    elems.forEach(function (el, i) {
-        const el_id = el.getAttribute('id');
-        let lbl = document.querySelector('label[for="' + el_id + '"]');
-        lbl.classList.add('checked');
-    });
-
-    elems = document.querySelectorAll('.flywork-labels .form-control');
-    elems.forEach(function (el, i) {
-        el.addEventListener('blur', function (evt) {
-            el.setAttribute('value', el.value);
+        elems = document.querySelectorAll('.flywork-labels .form-control');
+        elems.forEach(function (el, i) {
+            el.addEventListener('blur', function (evt) {
+                el.setAttribute('value', el.value);
+            });
         });
-    });
 
-    if (window.CustomEvent && typeof window.CustomEvent === 'function') {
-        var evtFocus = new CustomEvent('focus');
-    } else {
-        var evtFocus = document.createEvent('CustomEvent');
-        evtFocus.initCustomEvent('focus', true, true);
+        if (window.CustomEvent && typeof window.CustomEvent === 'function') {
+            var evtFocus = new CustomEvent('focus');
+        } else {
+            var evtFocus = document.createEvent('CustomEvent');
+            evtFocus.initCustomEvent('focus', true, true);
+        }
+
+        if (window.CustomEvent && typeof window.CustomEvent === 'function') {
+            var evtBlur = new CustomEvent('blur');
+        } else {
+            var evtBlur = document.createEvent('CustomEvent');
+            evtBlur.initCustomEvent('blur', true, true);
+        }
+        elems = document.querySelectorAll('.flywork-labels select.form-control');
+        elems.forEach(function (el, i) {
+            el.dispatchEvent(evtFocus);
+            el.dispatchEvent(evtBlur);
+        });
+
     }
 
-    if (window.CustomEvent && typeof window.CustomEvent === 'function') {
-        var evtBlur = new CustomEvent('blur');
-    } else {
-        var evtBlur = document.createEvent('CustomEvent');
-        evtBlur.initCustomEvent('blur', true, true);
+    return {
+        init: flywork_init
     }
-    elems = document.querySelectorAll('.flywork-labels select.form-control');
-    elems.forEach(function (el, i) {
-        el.dispatchEvent(evtFocus);
-        el.dispatchEvent(evtBlur);
-    });
-
 });
